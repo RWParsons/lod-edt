5e4aa12950111372d83f775bbc2982b5579debef
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2024-08-04 15:26:30 +1000

    ensure id variables are treated as categorical

--- R/wrangle-data.R
+++ R/wrangle-data.R
@@ -14,6 +14,12 @@ wrangle_data <- function(d_raw) {
     "last_contact"
   )
 
+  cols_factors <- c(
+    "study_no",
+    "hospital",
+    "sex"
+  )
+
   cols_keep <- c(
     "hos_los" = "hoslos",
     "troponin" = "zero_trop",
@@ -21,8 +27,8 @@ wrangle_data <- function(d_raw) {
     "presentation_no" = "presentation_number",
     "hospital_id" = "hospital",
     "pt_age",
-    "intervention",
     "pt_sex" = "sex",
+    "intervention",
     "days_since_site_start"
   )
 
@@ -33,14 +39,15 @@ wrangle_data <- function(d_raw) {
     mutate(across(any_of(dt_cols), ~ as.Date.character(.x, format = "%m/%d/%Y"))) |>
     mutate(
       pt_age = as.numeric(difftime(as.Date(arrival_date), as.Date(dob), units = "days")) / 365.25,
-      admit_days_since_2019 = as.numeric(difftime(arrival_date, ymd("2019-01-01"), units = "days"))
+      admit_days_since_2019 = as.numeric(difftime(arrival_date, ymd("2019-01-01"), units = "days")),
+      across(all_of(cols_factors), as.character)
     ) |>
     add_days_since_site_start() |>
-    select(cols_keep) |>
-    group_by(pt_id) |>
-    arrange(presentation_no) |>
-    fill(pt_age, pt_sex, .direction = "down") |>
+    group_by(study_no) |>
+    arrange(presentation_number) |>
+    fill(pt_age, sex, .direction = "down") |>
     ungroup() |>
+    select(all_of(cols_keep)) |>
     arrange(pt_id, presentation_no)
 }
 
--- _targets.R
+++ _targets.R
@@ -1,15 +1,11 @@
 source("dependencies.R")
 
-# Set target options:
 tar_option_set(
   controller = crew::crew_controller_local(workers = 2, seconds_idle = 60)
 )
 
-# Run the R scripts in the R/ folder with your custom functions:
 tar_source()
-# tar_source("other_functions.R") # Source other scripts as needed.
 
-# Replace the target list below with your own:
 list(
   tar_file(
     data_file,
