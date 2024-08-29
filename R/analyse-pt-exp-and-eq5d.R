da59b2ad022e225d7aa2ce6b312bebb2971ab0a2
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2024-08-29 13:59:41 +1000

    stratify eq5d and patient experience tables by cohorts

--- R/analyse-pt-exp-and-eq5d.R
+++ R/analyse-pt-exp-and-eq5d.R
@@ -13,30 +13,55 @@ visualise_eq5d <- function(data) {
 }
 
 get_qnt_eq5d <- function(data) {
-  prep_eq5d_data(data) |>
-    pivot_wider(names_from = domain, values_from = value)
+  list(
+    prep_eq5d_data(data, cohort = "full"),
+    prep_eq5d_data(data, cohort = "lod")  
+  ) |> 
+    map(~ pivot_wider(.x, names_from = domain, values_from = value)) |> 
+    bind_rows()
 }
 
-prep_eq5d_data <- function(data) {
+prep_eq5d_data <- function(data, cohort = c("full", "lod")) {
+  cohort <- match.arg(cohort)
+  troponin_lod <- get_troponin_lod()
   data |>
     filter(presentation_no == 1) |>
+    (\(d) {
+      if(cohort == "lod") {
+        d <- d |> filter(troponin <= troponin_lod)
+      }
+      d
+    })() |> 
     select(pt_id, intervention, starts_with("eq5d")) |>
     na.omit() |>
     pivot_longer(starts_with("eq5d"), names_to = "domain") |>
     group_by(intervention, domain) |>
     summarize(value = mean(value), n = n()) |>
     ungroup() |>
-    mutate(intervention = ifelse(intervention == 1, "Post-intervention", "Pre-intervention"))
+    mutate(
+      intervention = ifelse(intervention == 1, "Post-intervention", "Pre-intervention"),
+      cohort = .env$cohort
+    )
 }
 
 get_qnt_patient_experience <- function(data) {
+  troponin_lod <- get_troponin_lod()
   data |>
     filter(presentation_no == 1) |>
-    select(pt_id, intervention, starts_with("experience")) |>
-    na.omit() |>
-    pivot_longer(starts_with("experience"), names_to = "pt_exp_q") |>
-    group_by(intervention, pt_exp_q) |>
-    summarize(value = mean(value), n = n()) |>
-    ungroup() |>
-    pivot_wider(values_from = value, names_from = pt_exp_q)
+    (\(d) {
+      d_lod <- d |> filter(troponin <= troponin_lod) |> mutate(cohort = "lod")
+      d_full <- d |> mutate(cohort = "full")
+      list(d_lod, d_full)
+    })() |> 
+    map(
+      ~ .x |> 
+        select(pt_id, cohort, intervention, starts_with("experience")) |>
+        na.omit() |>
+        pivot_longer(starts_with("experience"), names_to = "pt_exp_q") |>
+        group_by(intervention, cohort, pt_exp_q) |>
+        summarize(value = mean(value), n = n()) |>
+        ungroup() |>
+        pivot_wider(values_from = value, names_from = pt_exp_q)
+    ) |> 
+    bind_rows()   
 }
